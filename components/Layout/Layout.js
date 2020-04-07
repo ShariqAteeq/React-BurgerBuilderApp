bea92fa5850e7142d97c121d8ea417624fe25af9
shariq <m.shariq22@hotmail.com>
2020-04-08 01:15:54 +0500

    Add toolbar with logo and navigation items

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const Layout = (props) => (
 
     <Aux>
-        <div>Toolbar, SideDrawer, BackDrop</div>
+        <Toolbar />
         <main className = {classes.Content}>
             {props.children}
         </main>
